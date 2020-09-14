420fe620dd462b5c48808868157572ae5e208dbb
Daniël de Kok <me@danieldk.eu>
2020-09-14 14:22:20 +0200

    dpar: init at 0.2.0
    This only packages the Rust parts of dpar.

--- default.nix
+++ default.nix
@@ -1,6 +1,10 @@
 { pkgs ? import (import ./nix/sources.nix).nixpkgs {} }:
 
-{
+rec {
+  dpar = pkgs.callPackage pkgs/dpar {
+    inherit libtensorflow;
+  };
+
   # Pin Tensorflow to our preferred version.
   libtensorflow = with pkgs; callPackage ./pkgs/libtensorflow {
     inherit (linuxPackages) nvidia_x11;
